42e5f57fd1c64a54326caefd9be56ed10d8544d6
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:53:29 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz39.cpp
@@ -0,0 +1,22 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры объем в байтах.Перевести его в килобайты, мегабайты, гигабайты и терабайты.
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double bytes;
+	cout << "Enter count of bytes: ";
+	cin >> bytes;
+	cout << endl << bytes << " bytes is:" << endl;
+	cout << "Bits = " << fixed << bytes * 8.0 << endl;
+	cout << "KBytes = " << fixed << setprecision(15) << bytes / 1024.0 << endl;
+	cout << "MBytes = " << fixed << setprecision(15) << bytes / (1024.0 * 1024.0) << endl;
+	cout << "GBytes = " << fixed << setprecision(15) << bytes / (1024.0 * 1024.0 * 1024.0) << endl;
+	cout << "TBytes = " << fixed << setprecision(15) << bytes / (1024.0 * 1024.0 * 1024.0 * 1024.0) << endl;
+
+	return 0;
+}
